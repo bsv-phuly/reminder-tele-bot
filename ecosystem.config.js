c56ecf6aaeb2e86756cc6a74dc93c203aff4e686
Ly Phu <phuly@bravesoft.com.vn>
2025-04-28 17:06:44 +0700

    update command run

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "telegram-bot",
     script: "dist/index.js",
-    instances: 1,
+    instances: "max",
     autorestart: true,
     watch: false,
     max_memory_restart: "500M",
